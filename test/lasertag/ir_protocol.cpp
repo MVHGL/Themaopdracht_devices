d6de6b99f4611a354dbbc4be765ca4f12a8b87d2
Nathan Hoekstra <nathan.hoekstra@student.hu.nl>
2018-10-05 10:32:52 +0200

    Added some code

--- test/lasertag/ir_protocol.cpp
+++ test/lasertag/ir_protocol.cpp
@@ -1,6 +1,9 @@
 #include "ir_protocol.hpp"
 
-ir_protocol::ir_protocol()
-{
-}
+ir_protocol::ir_protocol(hwlib::pin_in & reciever, hwlib::pin_out & transmitter):
+	reciever(reciever),
+	transmitter(transmitter)
+{};
+
+
 
--- test/lasertag/ir_protocol.hpp
+++ test/lasertag/ir_protocol.hpp
@@ -1,8 +1,13 @@
 #pragma once
+#include "hwlib.hpp"
 
 class ir_protocol
 {
+private:
+	hwlib::pin_in & reciever;		//The module's signal pin that gets the IR-signal
+	hwlib::pin_out & transmitter;	//The IR led that send's the signal
 public:
-	ir_protocol();
+	ir_protocol(hwlib::pin_in & reciever, hwlib::pin_out & transmitter);
+	void send(const int & player_id, const int & )
 };
 
