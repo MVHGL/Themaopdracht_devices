c5cacd2438aaedb22d22f84bd49e27d40e2c20dc
Ramon <ramon.vanbemmel@student.hu.nl>
2018-11-01 10:51:50 +0100

    Init game v0.3

--- Lasertag/initGame/main.cpp
+++ Lasertag/initGame/main.cpp
@@ -31,17 +31,85 @@ int main() {
 	//pin voor het verzenden van ir signal. 
 	auto IRLed= hwlib::target::pin_out(hwlib::target::pins::d5);
 	auto transmitter = ir_transmitter(IRLed); 
-
-	while(1){;
-		transmitter.send(10, 11) ; 
-		auto c = keypad.getc();
-		oled << "\f " << c << hwlib::flush;
+	
+	enum state_t= {IDLE, ADJUST_TTIME, BUTTON_PRESSED, SEND_IR_TIME, START_GAME};
+	state= state_t; 
+	state=IDLE;
+	int min_tientallen=0;
+	int min=0;
+	int time=0; 
+	while(1){
+		
+		
+		switch(state){
+		case IDLE: 
+			if (keypad.getc()=='C'){
+				state= ADJUST_TIME; 
+				break; 
+			}
+		case ADJUST_TIME: 
+			buttonTimer.set(20);
+			min_tientallen= int(keypad.getc()-48);
+			if (min_tientallen >=0 && min_tientallen <10)
+			{
+				min_tientallen *=10;
+				state= BUTTON_PRESSED_TWO; 
+				break
+			}
+			//als timer timed out is : state= idle 
+		case BUTTON_PRESSED_TWO: 
+			min = int(keypad.getc()-48); 
+			if (min >= 0 && min <10)
+			{
+				time= min_tientallen + min ; 
+				state= SEND_IR_TIME;
+				break; 
+			}
+			//als timer timed out is : state= idle 
+		case SEND_IR_TIME:
+			if (keypad.getc()=='#')
+			{
+				transmitter.send(0,time);
+			}
+			else if (keypad.getc()=='*')
+			{
+				state= START_GAME; 
+				break; 
+			}
+		case START_GAME:
+			transmitter.send(0,0); 
+			if(keypad.getc()=='d')
+			{
+				state=IDLE; 
+				break; 
+			}
+			else if(keypad.getc()=='*')
+			{
+				break; 
+			}
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		if (c =='C'){
 			while(1){
 				if(keypad.getc()=='#'){
 					transmitter.send(10, 10);
 					hwlib::wait_ms(100);
-					oled << "\f " << '#' << "\t0001" << "game" << "\t0002" << "initialised."<< hwlib::flush;
+					oled << "\f " << '#' << "\t0001" << "Geef spelduur:.." << "\t0002" << hwlib::flush;
+					min_tientallen = int((keypad.getc()-48))*10;
+					oled << "\f " << '#' << "\t0001" << "Geef spelduur:"<<min_tientallen<<"." << "\t0002" << hwlib::flush;
+					min= int((keypad.getc()-48));
+					oled << "\f " << '#' << "\t0001" << "Geef spelduur:"<<min_tientallen<<min <<"Druk op # om de tijd instellen" << "\t0002" << hwlib::flush;
+					if 
 				}
 				if(keypad.getc()=='*'){
 					oled << "\f " << '*' << "\t0002" <<"Nothing send."<<"\t0003" << "returning to menu." << hwlib::flush;
