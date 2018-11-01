94c3d6b5f1de60f2d8a8ea33477cf447819c66b0
Ramon <ramon.vanbemmel@student.hu.nl>
2018-11-01 14:09:40 +0100

    updated code init

--- Lasertag/initGame/initGame.cpp
+++ Lasertag/initGame/initGame.cpp
@@ -19,60 +19,69 @@ initGame::main() override{
 	while(1){
 		switch(state){
 		case IDLE: 
-			if (keypad.getc()=='C'){
+			wait(keypadChannel); 
+			if (keypadChannel.read()=='C'){
 				state= ADJUST_TIME; 
 				break; 
 			}
+			
 		case ADJUST_TIME: 
 			initTimer.set(20);
-			auto event = wait(initTimer);
-			min_tientallen= int(keypad.getc()-48);
-			if (min_tientallen >=0 && min_tientallen <10)
-			{
-				min_tientallen *=10;
-				state= BUTTON_PRESSED_TWO; 
-				break
+			auto event = wait(initTimer+keypadChannel);
+			if (event == keypadChannel){
+			min_tientallen= int(keypadChannel.read()-48);
+				if (min_tientallen >=0 && min_tientallen <10)
+				{
+					min_tientallen *=10;
+					state= BUTTON_PRESSED_TWO; 
+					break
+				}
 			}
 			else if (event== initTimer) 
 			{
 				state=IDLE;
 				break;
 			}
+			
 		case BUTTON_PRESSED_TWO: 
-			min = int(keypad.getc()-48); 
-			if (min >= 0 && min <10)
-			{
-				time= min_tientallen + min ; 
-				state= SEND_IR_TIME;
-				break; 
+			auto event = wait(initTimer+keypadChannel);
+			min = int(keypadChannel.read()-48); 
+			if(event==keypadChannel){
+				if (min >= 0 && min <10)
+				{
+					time= min_tientallen + min ; 
+					state= SEND_IR_TIME;
+					break; 
+				}
 			}
 			else if (event== initTimer) 
 			{
 				state=IDLE;
 				break;
 			} 
+			
 		case SEND_IR_TIME:
-			if (keypad.getc()=='#')
+			if (keypadChannel.read()=='#')
 			{
 				transmitter.send(0,time);
 			}
-			else if (keypad.getc()=='*')
+			else if (keypadChannel.read()=='*')
 			{
 				state= START_GAME; 
 				break; 
 			}
+			
 		case START_GAME:
 			transmitter.send(0,0); 
-			if(keypad.getc()=='D')
+			if(keypadChannel.read()=='D')
 			{
 				state=IDLE; 
 				break; 
 			}
-			else if(keypad.getc()=='*')
+			else if(keypadChannel.read()=='*')
 			{
 				break; 
 			}
 		}
-		
 	}
 }
